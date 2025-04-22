b96990743f247c9cc4f869ab2f8545672a0b804b
unknown <@ngknn.local>
2025-04-22 14:26:31 +0300

    Треугольник и Прямоугольник

---
+++ DLL_WithTeam_DBS/DLL_WithTeam_DBS/Triangle.cs
@@ -0,0 +1,15 @@
+﻿namespace DLL_Sistema_kontrolya_versiy
+{
+    public class Triangle
+    {
+        public static double P(double a, double b, double c)
+        {
+            return (a + b + c)/2;
+        }
+        public static double S(double a, double b, double c)
+        {
+            double Pl = (a + b + c)/2;
+            return Math.Sqrt(Pl*(Pl - a)*(Pl - b)*(Pl - c));
+        }
+    }
+}
